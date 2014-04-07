da22bbefb58034ce53536b514c80a582a4111e93
flyui <root@FLYDEV-K43SD.(none)>
2014-04-07 23:03:12 +0800

    first commit

---
+++ gen/cc/flydev/face/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cc.flydev.face;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/cc/flydev/launcher/settings/SettingsActivity.java
@@ -0,0 +1,47 @@
+package cc.flydev.launcher.settings;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.view.MenuItem;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
+
+import cc.flydev.face.R;
+
+public class SettingsActivity extends PreferenceActivity implements OnPreferenceClickListener
+{
+    private Preference mRestart;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+        
+        // Initialize the preferences
+        mRestart = findPreference(SettingsProvider.KEY_SETTINGS_RESTART);
+        mRestart.setOnPreferenceClickListener(this);
+        
+        // Show "up" button
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference)
+    {
+        if (preference == mRestart) {
+            // I am Launcher, so I will restart after killing
+            System.exit(0);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == android.R.id.home) finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
